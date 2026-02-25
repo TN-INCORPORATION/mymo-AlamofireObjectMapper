f69536300307433556080c6463419c949f8eeab3
Chanon Chaisakdanukool <Chanon.ch@tnds.com>
2026-02-25 12:27:23 +0700

    fix for 5.0

--- AlamofireObjectMapper/AlamofireObjectMapper.swift
+++ AlamofireObjectMapper/AlamofireObjectMapper.swift
@@ -2,102 +2,99 @@ import Foundation
 import Alamofire
 import ObjectMapper
 
+// MARK: - Serializers
+public struct ObjectMapperSerializer<T: BaseMappable>: ResponseSerializer {
+    public let keyPath: String?
+    public let object: T?
+    public let context: MapContext?
+
+    public init(keyPath: String?, object: T? = nil, context: MapContext? = nil) {
+        self.keyPath = keyPath
+        self.object = object
+        self.context = context
+    }
+
+    public func serialize(request: URLRequest?, response: HTTPURLResponse?, data: Data?, error: Error?) throws -> T {
+        if let error = error { throw error }
+        let jsonObject = DataRequest.processResponse(data: data, keyPath: keyPath)
+
+        if let object = object {
+            _ = Mapper<T>(context: context, shouldIncludeNilValues: false).map(JSONObject: jsonObject as? [String: Any], toObject: object)
+            return object
+        } else if let parsedObject = Mapper<T>(context: context, shouldIncludeNilValues: false).map(JSONObject: jsonObject as? [String: Any]) {
+            return parsedObject
+        }
+
+        throw AFError.responseSerializationFailed(reason: .decodingFailed(error: NSError(domain: "AlamofireObjectMapper", code: 2, userInfo: [NSLocalizedDescriptionKey: "ObjectMapper failed to serialize response."])))
+    }
+}
+
+public struct ObjectMapperArraySerializer<T: BaseMappable>: ResponseSerializer {
+    public let keyPath: String?
+    public let context: MapContext?
+
+    public init(keyPath: String?, context: MapContext? = nil) {
+        self.keyPath = keyPath
+        self.context = context
+    }
+
+    public func serialize(request: URLRequest?, response: HTTPURLResponse?, data: Data?, error: Error?) throws -> [T] {
+        if let error = error { throw error }
+        let jsonObject = DataRequest.processResponse(data: data, keyPath: keyPath)
+
+        if let parsedObject = Mapper<T>(context: context, shouldIncludeNilValues: false).mapArray(JSONObject: jsonObject as? [[String: Any]]) {
+            return parsedObject
+        }
+
+        throw AFError.responseSerializationFailed(reason: .decodingFailed(error: NSError(domain: "AlamofireObjectMapper", code: 2, userInfo: [NSLocalizedDescriptionKey: "ObjectMapper failed to serialize array response."])))
+    }
+}
+
+// MARK: - Immutable Serializers
+public struct ObjectMapperImmutableSerializer<T: ImmutableMappable>: ResponseSerializer {
+    public let keyPath: String?
+    public let context: MapContext?
+
+    public init(keyPath: String?, context: MapContext? = nil) {
+        self.keyPath = keyPath
+        self.context = context
+    }
+
+    public func serialize(request: URLRequest?, response: HTTPURLResponse?, data: Data?, error: Error?) throws -> T {
+        if let error = error { throw error }
+        let jsonObject = DataRequest.processResponse(data: data, keyPath: keyPath)
+
+        if let json = jsonObject as? [String: Any], let parsedObject = try? Mapper<T>(context: context).map(JSONObject: json) {
+            return parsedObject
+        }
+
+        throw AFError.responseSerializationFailed(reason: .decodingFailed(error: NSError(domain: "AlamofireObjectMapper", code: 2, userInfo: [NSLocalizedDescriptionKey: "ImmutableMapper failed to serialize response."])))
+    }
+}
+
+// MARK: - DataRequest Extension
 extension DataRequest {
-    
-    enum ErrorCode: Int {
-        case noData = 1
-        case dataSerializationFailed = 2
-    }
-    
-    internal static func processResponse(request: URLRequest?, response: HTTPURLResponse?, data: Data?, keyPath: String?) -> Any? {
-        if let data = data, let result = try? JSONSerialization.jsonObject(with: data, options: .allowFragments) {
-            if let keyPath = keyPath, !keyPath.isEmpty {
-                return (result as AnyObject).value(forKeyPath: keyPath)
-            }
-            return result
+    internal static func processResponse(data: Data?, keyPath: String?) -> Any? {
+        guard let data = data else { return nil }
+        let result = try? JSONSerialization.jsonObject(with: data, options: .allowFragments)
+        if let keyPath = keyPath, !keyPath.isEmpty {
+            return (result as AnyObject).value(forKeyPath: keyPath)
         }
-        return nil
-    }
-    
-    internal static func newError(_ code: ErrorCode, failureReason: String) -> NSError {
-        let errorDomain = "com.alamofireobjectmapper.error"
-        let userInfo = [NSLocalizedFailureReasonErrorKey: failureReason]
-        return NSError(domain: errorDomain, code: code.rawValue, userInfo: userInfo)
-    }
-
-    // MARK: - BaseMappable Serializers
-    
-    public static func ObjectMapperSerializer<T: BaseMappable>(_ keyPath: String?, mapToObject object: T? = nil, context: MapContext? = nil) -> DataResponseSerializer<T> {
-        return DataResponseSerializer { request, response, data, error in
-            if let error = error { return .failure(error) }
-            let JSONObject = processResponse(request: request, response: response, data: data, keyPath: keyPath)
-            
-            if let object = object {
-                _ = Mapper<T>(context: context, shouldIncludeNilValues: false).map(JSONObject: JSONObject as? [String: Any], toObject: object)
-                return .success(object)
-            } else if let parsedObject = Mapper<T>(context: context, shouldIncludeNilValues: false).map(JSONObject: JSONObject as? [String: Any]) {
-                return .success(parsedObject)
-            }
-            return .failure(newError(.dataSerializationFailed, failureReason: "ObjectMapper failed to serialize response."))
-        }
-    }
-    
-    public static func ObjectMapperArraySerializer<T: BaseMappable>(_ keyPath: String?, context: MapContext? = nil) -> DataResponseSerializer<[T]> {
-        return DataResponseSerializer { request, response, data, error in
-            if let error = error { return .failure(error) }
-            let JSONObject = processResponse(request: request, response: response, data: data, keyPath: keyPath)
-            
-            if let parsedObject = Mapper<T>(context: context, shouldIncludeNilValues: false).mapArray(JSONObject: JSONObject as? [[String: Any]]) {
-                return .success(parsedObject)
-            }
-            return .failure(newError(.dataSerializationFailed, failureReason: "ObjectMapper failed to serialize response."))
-        }
-    }
-
-    // MARK: - ImmutableMappable Serializers
-    
-    public static func ObjectMapperImmutableSerializer<T: ImmutableMappable>(_ keyPath: String?, context: MapContext? = nil) -> DataResponseSerializer<T> {
-        return DataResponseSerializer { request, response, data, error in
-            if let error = error { return .failure(error) }
-            let JSONObject = processResponse(request: request, response: response, data: data, keyPath: keyPath)
-            
-            if let JSONObject = JSONObject, let parsedObject = try? Mapper<T>(context: context, shouldIncludeNilValues: false).map(JSONObject: JSONObject as? [String: Any]) as T {
-                return .success(parsedObject)
-            }
-            return .failure(newError(.dataSerializationFailed, failureReason: "ObjectMapper failed to serialize response."))
-        }
-    }
-    
-    public static func ObjectMapperImmutableArraySerializer<T: ImmutableMappable>(_ keyPath: String?, context: MapContext? = nil) -> DataResponseSerializer<[T]> {
-        return DataResponseSerializer { request, response, data, error in
-            if let error = error { return .failure(error) }
-            if let JSONObject = processResponse(request: request, response: response, data: data, keyPath: keyPath),
-               let parsedObject = try? Mapper<T>(context: context, shouldIncludeNilValues: false).mapArray(JSONObject: JSONObject as? [[String: Any]]) as [T] {
-                return .success(parsedObject)
-            }
-            return .failure(newError(.dataSerializationFailed, failureReason: "ObjectMapper failed to serialize response."))
-        }
-    }
-
-    // MARK: - Handlers
-    
-    @discardableResult
-    public func responseObject<T: BaseMappable>(queue: DispatchQueue? = nil, keyPath: String? = nil, mapToObject object: T? = nil, context: MapContext? = nil, completionHandler: @escaping (DataResponse<T>) -> Void) -> Self {
-        return response(queue: queue, responseSerializer: DataRequest.ObjectMapperSerializer(keyPath, mapToObject: object, context: context), completionHandler: completionHandler)
-    }
-    
-    @discardableResult
-    public func responseArray<T: BaseMappable>(queue: DispatchQueue? = nil, keyPath: String? = nil, context: MapContext? = nil, completionHandler: @escaping (DataResponse<[T]>) -> Void) -> Self {
-        return response(queue: queue, responseSerializer: DataRequest.ObjectMapperArraySerializer(keyPath, context: context), completionHandler: completionHandler)
+        return result
     }
 
     @discardableResult
-    public func responseObject<T: ImmutableMappable>(queue: DispatchQueue? = nil, keyPath: String? = nil, context: MapContext? = nil, completionHandler: @escaping (DataResponse<T>) -> Void) -> Self {
-        return response(queue: queue, responseSerializer: DataRequest.ObjectMapperImmutableSerializer(keyPath, context: context), completionHandler: completionHandler)
+    public func responseObject<T: BaseMappable>(queue: DispatchQueue = .main, keyPath: String? = nil, mapToObject object: T? = nil, context: MapContext? = nil, completionHandler: @escaping (AFDataResponse<T>) -> Void) -> Self {
+        return response(queue: queue, responseSerializer: ObjectMapperSerializer(keyPath: keyPath, object: object, context: context), completionHandler: completionHandler)
     }
-    
+
     @discardableResult
-    public func responseArray<T: ImmutableMappable>(queue: DispatchQueue? = nil, keyPath: String? = nil, context: MapContext? = nil, completionHandler: @escaping (DataResponse<[T]>) -> Void) -> Self {
-        return response(queue: queue, responseSerializer: DataRequest.ObjectMapperImmutableArraySerializer(keyPath, context: context), completionHandler: completionHandler)
+    public func responseArray<T: BaseMappable>(queue: DispatchQueue = .main, keyPath: String? = nil, context: MapContext? = nil, completionHandler: @escaping (AFDataResponse<[T]>) -> Void) -> Self {
+        return response(queue: queue, responseSerializer: ObjectMapperArraySerializer(keyPath: keyPath, context: context), completionHandler: completionHandler)
+    }
+
+    @discardableResult
+    public func responseObject<T: ImmutableMappable>(queue: DispatchQueue = .main, keyPath: String? = nil, context: MapContext? = nil, completionHandler: @escaping (AFDataResponse<T>) -> Void) -> Self {
+        return response(queue: queue, responseSerializer: ObjectMapperImmutableSerializer(keyPath: keyPath, context: context), completionHandler: completionHandler)
     }
 }
--- AlamofireObjectMapperTests/AlamofireObjectMapperTests.swift
+++ AlamofireObjectMapperTests/AlamofireObjectMapperTests.swift
@@ -6,292 +6,65 @@ import Alamofire
 
 class AlamofireObjectMapperTests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-    }
-    
-    override func tearDown() {
-        super.tearDown()
-    }
+    let sampleURL = "https://raw.githubusercontent.com/tristanhimmelman/AlamofireObjectMapper/d8bb95982be8a11a2308e779bb9a9707ebe42ede/sample_json"
     
     func testResponseObject() {
-        let URL = "https://raw.githubusercontent.com/tristanhimmelman/AlamofireObjectMapper/d8bb95982be8a11a2308e779bb9a9707ebe42ede/sample_json"
-        let expectation = self.expectation(description: "\(URL)")
+        let expectation = self.expectation(description: "Get object")
 
-        _ = Alamofire.request(URL, method: .get).responseObject { (response: DataResponse<WeatherResponse>) in
+        // เปลี่ยนจาก Alamofire.request เป็น AF.request และ DataResponse เป็น AFDataResponse
+        AF.request(sampleURL, method: .get).responseObject { (response: AFDataResponse<WeatherResponse>) in
             expectation.fulfill()
             
-            let mappedObject = response.result.value
-            
-            XCTAssertNotNil(mappedObject, "Response should not be nil")
-            XCTAssertNotNil(mappedObject?.location, "Location should not be nil")
-            XCTAssertNotNil(mappedObject?.threeDayForecast, "ThreeDayForcast should not be nil")
-            
-            for forecast in mappedObject!.threeDayForecast! {
-                XCTAssertNotNil(forecast.day, "day should not be nil")
-                XCTAssertNotNil(forecast.conditions, "conditions should not be nil")
-                XCTAssertNotNil(forecast.temperature, "temperature should not be nil")
+            switch response.result {
+            case .success(let mappedObject):
+                XCTAssertNotNil(mappedObject.location)
+                XCTAssertNotNil(mappedObject.threeDayForecast)
+            case .failure(let error):
+                XCTFail("Request failed with error: \(error)")
             }
         }
-        
-        waitForExpectations(timeout: 10) { error in
-            XCTAssertNil(error, "\(String(describing: error))")
-        }
-    }
-    
-    func testResponseObjectMapToObject() {
-        let URL = "https://raw.githubusercontent.com/tristanhimmelman/AlamofireObjectMapper/d8bb95982be8a11a2308e779bb9a9707ebe42ede/sample_json"
-        let expectation = self.expectation(description: "\(URL)")
-        
-        let weatherResponse = WeatherResponse()
-        weatherResponse.date = Date()
-        
-        _ = Alamofire.request(URL, method: .get).responseObject(mapToObject: weatherResponse) { (response: DataResponse<WeatherResponse>) in
-            expectation.fulfill()
-            
-            let mappedObject = response.result.value
-            print(weatherResponse)
-            XCTAssertNotNil(mappedObject, "Response should not be nil")
-            XCTAssertNotNil(mappedObject?.date, "Date should not be nil")
-            XCTAssertNotNil(mappedObject?.location, "Location should not be nil")
-            XCTAssertNotNil(mappedObject?.threeDayForecast, "ThreeDayForcast should not be nil")
-            
-            for forecast in mappedObject!.threeDayForecast! {
-                XCTAssertNotNil(forecast.day, "day should not be nil")
-                XCTAssertNotNil(forecast.conditions, "conditions should not be nil")
-                XCTAssertNotNil(forecast.temperature, "temperature should not be nil")
-            }
-        }
-        
-        waitForExpectations(timeout: 10) { error in
-            XCTAssertNil(error, "\(String(describing: error))")
-        }
-    }
-    
-    func testResponseObjectWithKeyPath() {
-        let URL = "https://raw.githubusercontent.com/tristanhimmelman/AlamofireObjectMapper/2ee8f34d21e8febfdefb2b3a403f18a43818d70a/sample_keypath_json"
-        let expectation = self.expectation(description: "\(URL)")
-        
-        _ = Alamofire.request(URL, method: .get).responseObject(keyPath: "data") { (response: DataResponse<WeatherResponse>) in
-            expectation.fulfill()
-            
-            let mappedObject = response.result.value
-            
-            XCTAssertNotNil(mappedObject, "Response should not be nil")
-            XCTAssertNotNil(mappedObject?.location, "Location should not be nil")
-            XCTAssertNotNil(mappedObject?.threeDayForecast, "ThreeDayForcast should not be nil")
-            
-            for forecast in mappedObject!.threeDayForecast! {
-                XCTAssertNotNil(forecast.day, "day should not be nil")
-                XCTAssertNotNil(forecast.conditions, "conditions should not be nil")
-                XCTAssertNotNil(forecast.temperature, "temperature should not be nil")
-            }
-        }
-        
-        waitForExpectations(timeout: 10) { error in
-            XCTAssertNil(error, "\(String(describing: error))")
-        }
-    }
-    
-    func testResponseObjectWithNestedKeyPath() {
-        let URL = "https://raw.githubusercontent.com/tristanhimmelman/AlamofireObjectMapper/97231a04e6e4970612efcc0b7e0c125a83e3de6e/sample_keypath_json"
-        let expectation = self.expectation(description: "\(URL)")
-        
-        _ = Alamofire.request(URL, method: .get).responseObject(keyPath: "response.data") { (response: DataResponse<WeatherResponse>) in
-            expectation.fulfill()
-            
-            let mappedObject = response.result.value
-            
-            XCTAssertNotNil(mappedObject, "Response should not be nil")
-            XCTAssertNotNil(mappedObject?.location, "Location should not be nil")
-            XCTAssertNotNil(mappedObject?.threeDayForecast, "ThreeDayForcast should not be nil")
-            
-            for forecast in mappedObject!.threeDayForecast! {
-                XCTAssertNotNil(forecast.day, "day should not be nil")
-                XCTAssertNotNil(forecast.conditions, "conditions should not be nil")
-                XCTAssertNotNil(forecast.temperature, "temperature should not be nil")
-            }
-        }
-        
-        waitForExpectations(timeout: 10) { error in
-            XCTAssertNil(error, "\(String(describing: error))")
-        }
+        waitForExpectations(timeout: 10, handler: nil)
     }
 
     func testResponseArray() {
-        let URL = "https://raw.githubusercontent.com/tristanhimmelman/AlamofireObjectMapper/f583be1121dbc5e9b0381b3017718a70c31054f7/sample_array_json"
-        let expectation = self.expectation(description: "\(URL)")
+        let arrayURL = "https://raw.githubusercontent.com/tristanhimmelman/AlamofireObjectMapper/f583be1121dbc5e9b0381b3017718a70c31054f7/sample_array_json"
+        let expectation = self.expectation(description: "Get array")
 
-        _ = Alamofire.request(URL, method: .get).responseArray { (response: DataResponse<[Forecast]>) in
+        AF.request(arrayURL, method: .get).responseArray { (response: AFDataResponse<[Forecast]>) in
             expectation.fulfill()
             
-            let mappedArray = response.result.value
-            
-            XCTAssertNotNil(mappedArray, "Response should not be nil")
-            XCTAssertTrue(mappedArray?.count == 3, "Didn't parse correct amount of objects")
-            
-            for forecast in mappedArray! {
-                XCTAssertNotNil(forecast.day, "day should not be nil")
-                XCTAssertNotNil(forecast.conditions, "conditions should not be nil")
-                XCTAssertNotNil(forecast.temperature, "temperature should not be nil")
+            if case .success(let mappedArray) = response.result {
+                XCTAssertTrue(mappedArray.count == 3)
+            } else {
+                XCTFail("Parsing failed")
             }
         }
-
-        waitForExpectations(timeout: 10) { error in
-            XCTAssertNil(error, "\(String(describing: error))")
-        }
+        waitForExpectations(timeout: 10, handler: nil)
     }
-    
-    func testArrayResponseArrayWithKeyPath() {
-        let URL = "https://raw.githubusercontent.com/tristanhimmelman/AlamofireObjectMapper/d8bb95982be8a11a2308e779bb9a9707ebe42ede/sample_json"
-        let expectation = self.expectation(description: "\(URL)")
-        
-        _ = Alamofire.request(URL, method: .get).responseArray(keyPath: "three_day_forecast") { (response: DataResponse<[Forecast]>) in
-        
-            expectation.fulfill()
-            
-            let mappedArray = response.result.value
-            
-            XCTAssertNotNil(mappedArray, "Response should not be nil")
-            
-            for forecast in mappedArray! {
-                XCTAssertNotNil(forecast.day, "day should not be nil")
-                XCTAssertNotNil(forecast.conditions, "conditions should not be nil")
-                XCTAssertNotNil(forecast.temperature, "temperature should not be nil")
-            }
-        }
-        
-        waitForExpectations(timeout: 10) { error in
-            XCTAssertNil(error, "\(String(describing: error))")
-        }
-    }
-    
-    func testArrayResponseArrayWithNestedKeyPath() {
-        let URL = "https://raw.githubusercontent.com/tristanhimmelman/AlamofireObjectMapper/97231a04e6e4970612efcc0b7e0c125a83e3de6e/sample_keypath_json"
-        let expectation = self.expectation(description: "\(URL)")
-        
-        _ = Alamofire.request(URL, method: .get).responseArray(keyPath: "response.data.three_day_forecast") { (response: DataResponse<[Forecast]>) in
-            
-            expectation.fulfill()
-            
-            let mappedArray = response.result.value
-            
-            XCTAssertNotNil(mappedArray, "Response should not be nil")
-            
-            for forecast in mappedArray! {
-                XCTAssertNotNil(forecast.day, "day should not be nil")
-                XCTAssertNotNil(forecast.conditions, "conditions should not be nil")
-                XCTAssertNotNil(forecast.temperature, "temperature should not be nil")
-            }
-        }
-        
-        waitForExpectations(timeout: 10) { error in
-            XCTAssertNil(error, "\(String(describing: error))")
-        }
-    }
-    
-    // MARK: - Immutable Tests
     
     func testResponseImmutableObject() {
-        let URL = "https://raw.githubusercontent.com/tristanhimmelman/AlamofireObjectMapper/d8bb95982be8a11a2308e779bb9a9707ebe42ede/sample_json"
-        let expectation = self.expectation(description: "\(URL)")
-        
-        _ = Alamofire.request(URL, method: .get).responseObject { (response: DataResponse<WeatherResponseImmutable>) in
+        let expectation = self.expectation(description: "Get immutable object")
+
+        AF.request(sampleURL, method: .get).responseObject { (response: AFDataResponse<WeatherResponseImmutable>) in
             expectation.fulfill()
             
-            let mappedObject = response.result.value
-            
-            XCTAssertNotNil(mappedObject, "Response should not be nil")
-            XCTAssertNotNil(mappedObject?.location, "Location should not be nil")
-            XCTAssertNotNil(mappedObject?.threeDayForecast, "ThreeDayForcast should not be nil")
-            
-            for forecast in mappedObject!.threeDayForecast {
-                XCTAssertNotNil(forecast.day, "day should not be nil")
-                XCTAssertNotNil(forecast.conditions, "conditions should not be nil")
-                XCTAssertNotNil(forecast.temperature, "temperature should not be nil")
+            if let mappedObject = response.value {
+                XCTAssertNotNil(mappedObject.location)
+                XCTAssertFalse(mappedObject.threeDayForecast.isEmpty)
+            } else {
+                XCTFail("Immutable mapping failed")
             }
         }
-        
-        waitForExpectations(timeout: 10) { error in
-            XCTAssertNil(error, "\(String(describing: error))")
-        }
-    }
-    
-    func testResponseImmutableArray() {
-        let URL = "https://raw.githubusercontent.com/tristanhimmelman/AlamofireObjectMapper/f583be1121dbc5e9b0381b3017718a70c31054f7/sample_array_json"
-        let expectation = self.expectation(description: "\(URL)")
-        
-        _ = Alamofire.request(URL, method: .get).responseArray { (response: DataResponse<[ImmutableForecast]>) in
-            expectation.fulfill()
-            
-            let mappedArray = response.result.value
-            
-            XCTAssertNotNil(mappedArray, "Response should not be nil")
-            XCTAssertTrue(mappedArray?.count == 3, "Didn't parse correct amount of objects")
-            
-            for forecast in mappedArray! {
-                XCTAssertNotNil(forecast.day, "day should not be nil")
-                XCTAssertNotNil(forecast.conditions, "conditions should not be nil")
-                XCTAssertNotNil(forecast.temperature, "temperature should not be nil")
-            }
-        }
-        
-        waitForExpectations(timeout: 10) { error in
-            XCTAssertNil(error, "\(String(describing: error))")
-        }
-    }
-    
-}
-
-// MARK: - Response classes
-
-// MARK: - ImmutableMappable
-
-class ImmutableWeatherResponse: ImmutableMappable {
-    let location: String
-    let threeDayForecast: [ImmutableForecast]
-    
-    required init(map: Map) throws {
-        location = try map.value("location")
-        threeDayForecast = try map.value("three_day_forecast")
-    }
-
-    func mapping(map: Map) {
-        location >>> map["location"]
-        threeDayForecast >>> map["three_day_forecast"]
+        waitForExpectations(timeout: 10, handler: nil)
     }
 }
 
-class ImmutableForecast: ImmutableMappable {
-    let day: String
-    let temperature: Int
-    let conditions: String
-    
-    required init(map: Map) throws {
-        day = try map.value("day")
-        temperature = try map.value("temperature")
-        conditions = try map.value("conditions")
-    }
-    
-    func mapping(map: Map) {
-        day >>> map["day"]
-        temperature >>> map["temperature"]
-        conditions >>> map["conditions"]
-    }
-}
-
-// MARK: - Mappable
-
+// MARK: - Models (ตัวอย่าง)
 class WeatherResponse: Mappable {
     var location: String?
     var threeDayForecast: [Forecast]?
-    var date: Date?
     
-    init(){}
-    
-    required init?(map: Map){
-    }
-    
+    required init?(map: Map) {}
     func mapping(map: Map) {
         location <- map["location"]
         threeDayForecast <- map["three_day_forecast"]
@@ -303,46 +76,20 @@ class Forecast: Mappable {
     var temperature: Int?
     var conditions: String?
     
-    required init?(map: Map){
-    }
-    
+    required init?(map: Map) {}
     func mapping(map: Map) {
         day <- map["day"]
         temperature <- map["temperature"]
         conditions <- map["conditions"]
     }
 }
-    
+
 struct WeatherResponseImmutable: ImmutableMappable {
     let location: String
-    var threeDayForecast: [Forecast]
-    var date: Date?
+    let threeDayForecast: [Forecast]
     
     init(map: Map) throws {
         location = try map.value("location")
         threeDayForecast = try map.value("three_day_forecast")
-    }
-    
-    func mapping(map: Map) {
-        location >>> map["location"]
-        threeDayForecast >>> map["three_day_forecast"]
-    }
-}
-
-struct ForecastImmutable: ImmutableMappable {
-    let day: String
-    var temperature: Int
-    var conditions: String?
-    
-    init(map: Map) throws {
-        day = try map.value("day")
-        temperature = try map.value("temperature")
-        conditions = try? map.value("conditions")
-    }
-    
-    func mapping(map: Map) {
-        day >>> map["day"]
-        temperature >>> map["temperature"]
-        conditions >>> map["conditions"]
     }
 }
